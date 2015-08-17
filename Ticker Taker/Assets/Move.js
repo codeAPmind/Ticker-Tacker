59b70fbfb00cc91a4ae3c592adb9020582b093bc
codeAPmind <himilong@yeah.net>
2015-08-17 21:58:24 +0800

    unity3D demo
    a simple unity demo : padlle and ball

---
+++ Ticker Taker/Assets/Move.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var smooth = 5.0;
+var tiltAngle = 30.0;
+function Start () {
+
+}
+
+function Update () {
+	var halfW:float = Screen.width/2;
+	transform.position.x = (Input.mousePosition.x - halfW)/halfW;
+	var halfH:float = Screen.height/2;
+	transform.position.z = (Input.mousePosition.y - halfH)/halfH;
+	
+	//Smoothly tilts a transform towards a target ratation
+	var tiltAroundZ = Input.GetAxis("Mouse X") * tiltAngle;
+	var tiltAroundX = Input.GetAxis("Mouse Y")*tiltAngle;
+	var target = Quaternion.Euler(tiltAroundX,0,tiltAroundZ);
+	
+	//Dampem towards the target rotation
+	transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+	Debug.Log((Input.mousePosition.x - Screen.width/2)/Screen.width/2);
+	//transform.position.x = Input.mousePosition.x;
+	//Debug.Log(Input.mousePosition.x);
+}
